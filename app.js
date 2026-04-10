bdef1bd2c45a48d804839e4301dafef38624e8be
irfan-nadapuram <MYGB131@GMAIL.COM>
2026-04-10 14:25:29 +0530

    third

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 
 // Server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
